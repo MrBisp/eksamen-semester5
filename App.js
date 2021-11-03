b5dd1a2e2ff0198e9ee0256554ec44e334a04b29
toni19ab <toni19ab@student.cbs.dk>
2021-11-03 13:36:00 +0100

    Tilføjelse af søg til navigationsbaren

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import StackNavigator from "./components/StackNavigator";
 import { Ionicons } from '@expo/vector-icons';
 import Recipe from "./components/Recipe";
 import UploadScreen from "./components/UploadScreen";
+import SearchScreen from "./components/UploadScreen";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -23,9 +24,10 @@ export default function App() {
                         iconName = focused ? 'ios-home' : 'ios-home-outline';
                     } else if (route.name === 'Settings') {
                         iconName = focused ? 'ios-list' : 'ios-list';
-                    }
-                    else if (route.name === 'Upload opskrift') {
+                    } else if (route.name === 'Upload opskrift') {
                         iconName = focused ? 'ios-add' : 'ios-add-outline';
+                    } else if (route.name === 'Søg') {
+                        iconName = focused ? 'ios-search' : 'ios-search';
                     }
 
 
@@ -37,7 +39,8 @@ export default function App() {
             })}
         >
             <Tab.Screen name={'Dit feed'} component={StackNavigator} options={{headerShown: false}}/>
-            <Tab.Screen name={'Upload opskrift'} component={UploadScreen} options={{headerShown: false}}/>
+            <Tab.Screen name={'Søg'} component={UploadScreen} options={{headerShown: false}}/>
+            <Tab.Screen name={'Upload opskrift'} component={SearchScreen} options={{headerShown: false}}/>
             <Tab.Screen name={'Settings'} component={SettingsScreen} options={{headerShown: false}} />
         </Tab.Navigator>
       </NavigationContainer>
