ed03aca0531010c82e9aa1dab913b10aeb23d5a7
Reuther <55132011+Reuther@users.noreply.github.com>
2021-11-10 21:27:35 +0100

    Tilføjet ProfileScreen.js

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,21 +1,45 @@
 import React, {useEffect, useState} from "react";
-import {SafeAreaView, ScrollView, View, Text, TextInput, Button, StyleSheet, Alert} from "react-native";
+import {
+    SafeAreaView,
+    ScrollView,
+    View,
+    Text,
+    TextInput,
+    Button,
+    StyleSheet,
+    Alert,
+    ImageBackground
+} from "react-native";
+import ImageFrederik from "../assets/frederik.jpg"
 import firebase from "firebase/compat";
 
 const ProfileScreen = (navigation) => {
-
-    const [recipes, setRecipes] = useState()
-
-    useEffect(() => {
-        if(!recipes)
-        {
-            firebase.database().ref()
-        }
-    })
     return(
-        <TextInput>Min Profil</TextInput>
-    )
+      <SafeAreaView style={styles.container}>
+          <ScrollView>
+        <Text style={styles.h1}>Min Profil</Text>
+              <ImageBackground source={ImageFrederik} style={{width:150, height:150}} />
+          </ScrollView>
+      </SafeAreaView>
+          )
 }
 
 
 export default ProfileScreen
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop:100
+    },
+    h1: {
+        fontSize: 42,
+        fontWeight: '600',
+        paddingLeft: 20
+    },
+    recipes: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+
+});
